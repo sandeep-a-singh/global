62074b1d8042c739fb22445e0a2efd8dddf10feb
sunny080593 <sunny9969888992@gmail.com>
2021-05-09 17:47:35 +0530

    RQ-4-Add_Logger_to_Application

--- target/src/main/java/com/sunny/enterprise/target/TargetApplication.java
+++ target/src/main/java/com/sunny/enterprise/target/TargetApplication.java
@@ -1,13 +1,18 @@
 package com.sunny.enterprise.target;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TargetApplication {
-
+	private static final Logger LOGGER = LogManager.getLogger(TargetApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TargetApplication.class, args);
+
+		LOGGER.info("Application Started "+ TargetApplication.class.getCanonicalName());
 	}
 
 }
